b43e42aaee27fd9f26306e3c4318e94276504c13
sourav verma <sourav.verma@ttu.edu>
2020-11-12 11:44:40 -0800

    created sample react app

---
+++ react-fb/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome Glad you Came!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
