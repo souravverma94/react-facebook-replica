84b895c650540768ba396b403d4686ec7fda6024
sourav verma <sourav.verma@ttu.edu>
2020-11-12 22:07:52 -0800

    fb logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import renderIf from "render-if";
 import "./App.css";
-import SignInUsers from "./services/signIn";
 import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
+//import SignIn from "./components/SignIn";
 import Feed from "./components/Feed";
 import NavBar from "./components/NavBar";
 import { firebaseApp } from "./firebase";
@@ -11,20 +10,6 @@ import { firebaseApp } from "./firebase";
 const App = () => {
   const [userStatus, setUserStatus] = useState(false);
   useEffect(() => {}, []);
-
-  const handleSignup = () => {
-    const email = "abc@gmail.com";
-    const password = "123456789";
-    const res = CreateUser(email, password, "sourav", "verma");
-    console.log(res);
-  };
-
-  const handleSignIn = () => {
-    const email = "abc@gmail.com";
-    const password = "123456789";
-    const res = SignInUsers(email, password);
-    console.log(res);
-  };
 
   firebaseApp.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -43,8 +28,6 @@ const App = () => {
       <NavBar userStatus={userStatus} />
       {renderIf(userStatus)(<Feed />)}
       {renderIf(!userStatus)(<SignUp />)}
-      <button onClick={handleSignup}>Sign Up</button>
-      <button onClick={handleSignIn}>Sign In</button>
     </div>
   );
 };
